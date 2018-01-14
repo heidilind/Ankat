e19f45d1ec54da21803da7150f4cf3d92c6838ed
Heidi <heidi.lindroos@aalto.fi>
2018-01-14 14:49:06 +0200

    Fix Sighting data model

--- app/model/Sighting.scala
+++ app/model/Sighting.scala
@@ -1,13 +1,11 @@
 package model
 
+import javax.inject.Inject
+import play.api.db.slick.{DatabaseConfigProvider, HasDatabaseConfigProvider}
+import play.db.NamedDatabase
 import slick.jdbc.PostgresProfile.api._
-import slick.driver.PostgresDriver.api._
-import play.api.Play
-import play.api.db.slick.DatabaseConfigProvider
-import scala.concurrent.Future
-import scala.concurrent.ExecutionContext.Implicits.global
-import slick.driver.JdbcProfile
-import play.api.db.slick.HasDatabaseConfigProvider
+
+import scala.concurrent.{ExecutionContext, Future}
 
 
 case class Sighting( 
@@ -25,28 +23,28 @@ class SightingTable(tag: Tag) extends Table[Sighting](tag, "sighting") {
   
 }
 
-object Sightings extends HasDatabaseConfigProvider[JdbcProfile]  {
-
-  val dbConfig = DatabaseConfigProvider
+class SightingStorage @Inject() (@NamedDatabase("mydb") protected val dbConfigProvider: DatabaseConfigProvider)
+                                 (implicit executionContext: ExecutionContext)
+                                  extends HasDatabaseConfigProvider[slick.jdbc.JdbcProfile] {
 
   val sightings = TableQuery[SightingTable]
 
   def add(sight: Sighting): Future[String] = {
-    dbConfig.db.run(sightings += sight).map(res => "User successfully added").recover {
+    db.run(sightings += sight).map(res => "User successfully added").recover {
       case ex: Exception => ex.getCause.getMessage
     }
   }
 
   def delete(id: Long): Future[Int] = {
-    dbConfig.db.run(sightings.filter(_.id === id).delete)
+    db.run(sightings.filter(_.id === id).delete)
   }
 
   def get(id: Long): Future[Option[Sighting]] = {
-    dbConfig.db.run(sightings.filter(_.id === id).result.headOption)
+    db.run(sightings.filter(_.id === id).result.headOption)
   }
 
   def listAll: Future[Seq[Sighting]] = {
-    dbConfig.db.run(sightings.result)
+    db.run(sightings.result)
   }
 
 }
