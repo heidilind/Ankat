5a07288e9349fc85d9ac7813551fc56d147bf027
Heidi <heidi.lindroos@aalto.fi>
2018-01-26 13:03:38 +0200

    Add database initialization

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -1,9 +1,26 @@
 # --- !Ups
+
+CREATE TABLE "specie" (
+	"id" SERIAL,
+	"name" VARCHAR(20) UNIQUE
+);
+
+INSERT INTO "specie" ("id", "name") VALUES 
+	(1, 'mallard'),
+	(2, 'redhead'),
+	(3, 'gadwall'),
+	(4, 'canvasback'),
+	(5, 'lesser scaup');
+
 CREATE TABLE "sighting" (
 	"id" SERIAL,
 	"description" TEXT,
-	"count" INTEGER
+	"specie" VARCHAR(20) references specie(name),
+	"count" INTEGER,
+	"ts" TIMESTAMP
 );
+
 
 # --- !Downs
 DROP TABLE "sighting";
+DROP TABLE "specie";
