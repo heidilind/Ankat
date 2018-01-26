96c4d2b6cbc3460257b9ace6a3d103471fe13333
Heidi <heidi.lindroos@aalto.fi>
2018-01-26 13:06:00 +0200

    Rework tests

--- test/controllers/HomeControllerSpec.scala
+++ test/controllers/HomeControllerSpec.scala
@@ -6,7 +6,7 @@ import play.api.test._
 import play.api.test.Helpers._
 import play.api.libs.json.Json
 import play.mvc._
- 
+
 /**
  * Add your spec here.
  * You can mock out a whole application including requests, plugins etc.
@@ -14,77 +14,99 @@ import play.mvc._
  * For more information, see https://www.playframework.com/documentation/latest/ScalaTestingWithScalaTest
  */
 class HomeControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injecting {
- 
+
   "SightingsController GET" should {
- 
+
     "render the index page from a new instance of controller" in {
       val controller = inject[SightingsController]
       val home = controller.index().apply(FakeRequest(GET, "/"))
- 
+
       status(home) mustBe OK
       contentType(home) mustBe Some("text/html")
       contentAsString(home) must include("Welcome to Play")
     }
- 
+
     "render the index page from the application" in {
       val controller = inject[SightingsController]
       val home = controller.index().apply(FakeRequest(GET, "/"))
- 
+
       status(home) mustBe OK
       contentType(home) mustBe Some("text/html")
       contentAsString(home) must include("Welcome to Play")
     }
- 
+
     "render the index page from the router" in {
       val request = FakeRequest(GET, "/").withHeaders(FakeHeaders(Seq((Http.HeaderNames.HOST, "localhost:9000"))))
       val home = route(app, request).get
- 
+
       status(home) mustBe OK
       contentType(home) mustBe Some("text/html")
       contentAsString(home) must include("Welcome to Play")
     }
- 
+
     "render the sightings page from the router" in {
       val request = FakeRequest(GET, "/sightings").withHeaders(FakeHeaders(Seq((Http.HeaderNames.HOST, "localhost:9000"))))
       val home = route(app, request).get
- 
+
       status(home) mustBe OK
-      contentType(home) mustBe Some("application/json")
+      contentType(home) mustBe Some("text/plain")
+
       contentAsString(home) must include("id")
       contentAsString(home) must include("description")
-      contentAsString(home) must include("species")
-      contentAsString(home) must include("dateTime")
+      contentAsString(home) must include("specie")
+      contentAsString(home) must include("sighting made")
       contentAsString(home) must include("count")
+
     }
- 
+
     "render the report sightings page from the router" in {
       val url = "/sightings/report"
       val request = FakeRequest(POST, controllers.routes.SightingsController.reportSightings.url).withHeaders(FakeHeaders(Seq((Http.HeaderNames.HOST, "localhost:9000"))))
       val Some(result) =
         route(app, request.withJsonBody(
           Json.obj(
-            "id" -> "1",
+            "id" -> 1,
             "species" -> "redhead",
             "description" -> "very red head",
             "dateTime" -> "2017-10-01T01:01:00Z",
             "count" -> 8)))
- 
+
       status(result) mustBe OK
       contentType(result) mustBe Some("application/json")
-      contentAsString(result) must include("The request contained JSON data")
-      contentAsString(result) must include("id")
+      contentAsString(result) must include("User successfully added")
+
     }
- 
-    "render the report NO sightings page from the router" in {
+
+    "render the report sightings page without sighting from the router" in {
       val url = "/sightings/report"
       val request = FakeRequest(POST, controllers.routes.SightingsController.reportSightings.url).withHeaders(FakeHeaders(Seq((Http.HeaderNames.HOST, "localhost:9000"))))
-      val Some(result) = route(app, request.withTextBody(""))
-     
-      status(result) mustBe OK
+      val Some(result) = 
+        route(app, request.withJsonBody(Json.obj()))
+
+      status(result) mustBe 400
       contentType(result) mustBe Some("application/json")
-      contentAsString(result) must include("The request contained no JSON data")
- 
+      contentAsString(result) must include("error.path.missing")
+
     }
- 
+
+    "render the report sightings with wrong specie page from the router" in {
+      val url = "/sightings/report"
+      val request = FakeRequest(POST, controllers.routes.SightingsController.reportSightings.url).withHeaders(FakeHeaders(Seq((Http.HeaderNames.HOST, "localhost:9000"))))
+      val Some(result) = 
+        route(app, request.withJsonBody(
+          Json.obj(
+            "id" -> 1,
+            "species" -> "bluehead",
+            "description" -> "very red head",
+            "dateTime" -> "2017-10-01T01:01:00Z",
+            "count" -> 8)))
+            
+      status(result) mustBe 500
+      contentType(result) mustBe Some("application/json")
+      contentAsString(result) must include("\"status\":\"Internal Service error\"")
+      contentAsString(result) must include("java.lang.NullPointerException")
+
+    }
+
   }
 }
